bea4cf41b194ab8dcc5660b4a8dde6b20013ded0
andres <andreshuertasgon@gmail.com>
2019-03-25 23:41:55 -0500

    Second Release Individual

---
+++ Second_release/src/app.js
@@ -0,0 +1,57 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+const hbs = require('hbs');
+const bodyParser = require('body-parser');
+
+const dirNode_modules = path.join(__dirname , '../node_modules')
+app.use('/css', express.static(dirNode_modules + '/bootstrap/dist/css'));
+app.use('/js', express.static(dirNode_modules + '/jquery/dist'));
+app.use('/js', express.static(dirNode_modules + '/popper.js/dist'));
+app.use('/js', express.static(dirNode_modules + '/bootstrap/dist/js'));
+
+require('./helpers');
+
+const directoriopublico = path.join(__dirname, '../public');
+const directoriopartials = path.join(__dirname, '../partials');
+app.use(express.static(directoriopublico));
+hbs.registerPartials(directoriopartials);
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.set('view engine', 'hbs');
+
+app.get('/',(req,res) => {
+    res.render('index', {
+        estudiante: 'Andrés Huertas'
+    })
+});
+
+app.post('/calculos', (req, res) =>{
+    res.render('calculos', {
+        estudiante: req.body.nombre, 
+        nota1: parseInt(req.body.nota1), 
+        nota2: parseInt(req.body.nota2), 
+        nota3: parseInt(req.body.nota3)
+    })
+});
+
+app.post('/crearcurso', (req, res) =>{
+    res.render('crearcurso', {
+        id: parseInt(req.body.id), 
+        nombre: req.body.nombre, 
+        modalidad: req.body.modalidad, 
+        valor: parseInt(req.body.valor), 
+        descripcion: req.body.descripcion, 
+        intensidad: parseInt(req.body.intensidad)
+    })
+});
+
+app.get('*', (req, res) => {
+    res.render('error',  {
+        estudiante: 'error'
+    })
+});
+
+app.listen(3000, () => {
+    console.log('Escuchando en el puerto 3000')
+});
---
+++ Second_release/src/helpers.js
@@ -0,0 +1,96 @@
+const hbs = require('hbs');
+const fs = require('fs');
+listaCursos= []
+// const {funcionCursos} = require('./funciones_cursos');
+
+hbs.registerHelper('obtenerPromedio', (nota1, nota2, nota3) =>{
+    return (nota1+nota2+nota3)/3
+})
+
+hbs.registerHelper('crearCurso', 
+        (id_p, nombre_p, modalidad_p, valor_p, descripcion_p, intensidad_p) =>{
+    let curso = {
+        id: id_p, 
+        nombre: nombre_p, 
+        modalidad: modalidad_p, 
+        valor: valor_p, 
+        descripcion: descripcion_p, 
+        intensidad: intensidad_p, 
+        estado: 'Disponible'
+    } 
+
+    let texto = crearCurso(curso);
+    // let texto = funcionCursos.crearCurso(curso);
+    return (texto)
+});
+
+hbs.registerHelper('listarCursos', () =>{
+    listarCursos();
+    let texto = "<table>\
+    <thead'>\
+        <th>id</th>\
+        <th>Nombre</th>\
+        <th>Descripción</th>\
+        <th>Valor</th>\
+        <th>Modalidad</th>\
+        <th>Intensidad</th>\
+        <th>Estado</th>\
+    </thead>\
+    <tbody>";
+
+    listaCursos.forEach(ech_curso => {
+        texto = texto +
+            '<tr>' +
+            '<td>' + ech_curso.id + '</td>' +
+            '<td>' + ech_curso.nombre + '</td>' +
+            '<td>' + ech_curso.descripcion + '</td>' +
+            '<td>' + ech_curso.valor + '</td>' +
+            '<td>' + ech_curso.modalidad + '</td>' +
+            '<td>' + ech_curso.intensidad + '</td>' +
+            '<td>' + ech_curso.estado + '</td>' +
+            '<tr>';
+    })
+
+    texto = texto + '</tbody> </table>';
+    return texto;
+});
+
+const crearCurso = (curso) => {
+    listarCursos();
+    let cur ={
+        id: curso.id, 
+        nombre: curso.nombre, 
+        modalidad: curso.modalidad, 
+        valor: curso.valor, 
+        descripcion: curso.descripcion, 
+        intensidad: curso.intensidad,
+        estado: curso.estado
+    };
+    //control de cursos duplicados
+    let cursoDuplicado = listaCursos.find (curs => curs.id == cur.id)
+    let mensaje;
+    if (!cursoDuplicado){
+        listaCursos.push(cur);
+        guardarCurso();
+        mensaje = 'Se ha creado el curso '+ cur.nombre +' de manera exitosa.';
+    } else{
+        mensaje = 'Ya existe otro curso con ese ID: ' + cur.id ;
+    }
+    return mensaje;
+}
+
+const listarCursos = () => {
+    try{
+        listaCursos = require('./../listado_Cursos.json');  // Lista de forma sincrona
+    } catch(error){
+        listaCursos = [];
+    }
+}
+
+const guardarCurso = () => {
+    let datos = JSON.stringify(listaCursos);
+    fs.writeFile('listado_Cursos.json', datos, (err)=>{
+        if (err) throw (err);
+        console.log('archivo creado con éxito');
+    })
+}
